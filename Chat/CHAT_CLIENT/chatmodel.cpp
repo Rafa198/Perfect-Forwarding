e90dcff53c02d29887401b2dc7d5e519f100dcb0
Rafael Fazlyev <rafa198@inbox.ru>
2018-12-12 15:56:50 +0300

    init

---
+++ Chat/CHAT_CLIENT/chatmodel.cpp
@@ -0,0 +1,46 @@
+#include "chatmodel.h"
+
+ChatModel::ChatModel(QObject *parent) :
+  QAbstractListModel(parent)
+{
+
+}
+
+int ChatModel::rowCount(const QModelIndex &parent) const
+{
+  if(parent.isValid())
+    {
+      return 0;
+    }
+
+  return modelData.size();
+}
+
+QVariant ChatModel::data(const QModelIndex &index, int role) const
+{
+  if(!index.isValid())
+    {
+      return QVariant();
+    }
+
+  const ModelData& data = modelData[index.row()];
+
+  switch (role) {
+    case ChatModel::Roles::UserNameRole:
+      return QString::fromStdString(data.userName);
+    case ChatModel::Roles::MessageRole:
+      return QString::fromStdString(data.message);
+    default:
+        return QVariant();
+    }
+}
+
+QHash<int, QByteArray> ChatModel::roleNames() const
+{
+  QHash<int, QByteArray> roles = QAbstractListModel::roleNames();
+
+  roles[UserNameRole] = "userName";
+  roles[MessageRole]  = "message";
+
+  return roles;
+}
---
+++ Chat/CHAT_CLIENT/chatmodel.h
@@ -0,0 +1,44 @@
+#ifndef CHATMODEL_H
+#define CHATMODEL_H
+
+#include <QObject>
+#include <QAbstractListModel>
+#include <QString>
+#include <QList>
+
+#include <list>
+#include <string>
+
+class ChatModel : public QAbstractListModel
+{
+  Q_OBJECT
+public:
+  ChatModel(QObject *parent = nullptr);
+
+  enum Roles {
+    UserNameRole = Qt::UserRole + 1,
+    MessageRole
+  };
+
+  virtual int rowCount(const QModelIndex &parent) const;
+  virtual QVariant data(const QModelIndex &index, int role) const;
+  virtual QHash<int, QByteArray> roleNames() const;
+
+signals:
+  void dataChanged();
+
+public slots:
+
+
+private:
+
+    struct ModelData {
+    std::string userName;
+    std::string message;
+  };
+
+  QList<ModelData> modelData;
+  //QString recipient;
+};
+
+#endif // CHATMODEL_H
